1fa5ebb29545eea1fe12920b9dfe60c3688e4852
zcassini <zcassini@gmail.com>
2021-06-02 10:17:49 -0400

    clean up tests a lil

--- src/test/java/org/wecancoeit/reviews/ReviewRepositoryTest.java
+++ src/test/java/org/wecancoeit/reviews/ReviewRepositoryTest.java
@@ -8,19 +8,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReviewRepositoryTest {
+    ReviewRepository underTest;
     Review reviewOne = new Review(1L, "Smalltalk");
     Review reviewTwo = new Review(2L, "Clojure");
 
     @Test
     public void shouldFindReviewOne() {
-        ReviewRepository underTest = new ReviewRepository(reviewOne);
+        underTest = new ReviewRepository(reviewOne);
         Review foundReview = underTest.findOne(1L);
         assertEquals(reviewOne, foundReview);
     }
 
     @Test
     public void shouldFindReviewOneAndReviewTwo() {
-        ReviewRepository underTest = new ReviewRepository(reviewOne, reviewTwo);
+        underTest = new ReviewRepository(reviewOne, reviewTwo);
         Collection<Review> foundReviews = underTest.findAll();
         assertThat(foundReviews).contains(reviewOne, reviewTwo);
     }
